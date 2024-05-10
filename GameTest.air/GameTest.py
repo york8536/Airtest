7d6ac88f331f69fd087eee7c3ebf51db5e55f21d
york8536 <york8536@gmail.com>
2024-05-10 09:51:47 +0800

    20240510
    新增優化進入遊戲函式

--- GameTest.air/GameTest.py
+++ GameTest.air/GameTest.py
@@ -137,6 +137,99 @@ def ClickGame(gamecode):
         print("進入遊戲[10038]")
         while exists(Template(r"tpl1715239401696.png", record_pos=(-0.001, 0.181), resolution=(2400, 1080))):
             touch((1200,960))
+def ClickGameNew(gamecode):
+    match gamecode:
+        case "10001":
+            while not exists(Template(r"tpl1714698666528.png", record_pos=(0.013, -0.033), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714698666528.png", record_pos=(0.013, -0.033), resolution=(2400, 1080)))
+            print("進入遊戲[10001]")
+        
+        case "10002":
+            while not exists(Template(r"tpl1714645388655.png", record_pos=(0.055, 0.104), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714645388655.png", record_pos=(0.055, 0.104), resolution=(2400, 1080)))
+            print("進入遊戲[10002]")
+            
+        case "10005":
+            while not exists(Template(r"tpl1714698541152.png", record_pos=(0.177, -0.03), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714698541152.png", record_pos=(0.177, -0.03), resolution=(2400, 1080)))
+            print("進入遊戲[10005]")
+        
+        case "10006":
+            while not exists(Template(r"tpl1714698738316.png", record_pos=(0.145, -0.045), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714698738316.png", record_pos=(0.145, -0.045), resolution=(2400, 1080)))
+            print("進入遊戲[10006]")
+        
+        case "10007":
+            while not exists(Template(r"tpl1714711017065.png", record_pos=(0.075, 0.091), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714711017065.png", record_pos=(0.075, 0.091), resolution=(2400, 1080)))
+            print("進入遊戲[10007]")
+        
+        case "10008":
+            while not exists(Template(r"tpl1714698990255.png", record_pos=(0.114, 0.105), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714698990255.png", record_pos=(0.114, 0.105), resolution=(2400, 1080)))
+            print("進入遊戲[10008]")
+        
+        case "10010":
+            while not exists(Template(r"tpl1714699101986.png", record_pos=(-0.097, 0.104), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714699101986.png", record_pos=(-0.097, 0.104), resolution=(2400, 1080)))
+            print("進入遊戲[10010]")
+        
+        case "10011":
+            while not exists(Template(r"tpl1714700634343.png", record_pos=(0.288, -0.035), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714700634343.png", record_pos=(0.288, -0.035), resolution=(2400, 1080)))
+            print("進入遊戲[10011]")
+        
+        case "10012":
+            while not exists(Template(r"tpl1714699055247.png", record_pos=(0.11, -0.046), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714699055247.png", record_pos=(0.11, -0.046), resolution=(2400, 1080)))
+            print("進入遊戲[10012]")
+        
+        case "10014":
+            while not exists(Template(r"tpl1714700769049.png", record_pos=(0.19, 0.101), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714700769049.png", record_pos=(0.19, 0.101), resolution=(2400, 1080)))
+            print("進入遊戲[10014]")
+        
+        case "10016":
+            while not exists(Template(r"tpl1714700892997.png", record_pos=(0.422, -0.046), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714700892997.png", record_pos=(0.422, -0.046), resolution=(2400, 1080)))
+            print("進入遊戲[10016]")
+        
+        case "10021":
+            while not exists(Template(r"tpl1714701153966.png", record_pos=(-0.021, 0.103), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714701153966.png", record_pos=(-0.021, 0.103), resolution=(2400, 1080)))
+            print("進入遊戲[10021]")
+        
+        case "10022":
+            while not exists(Template(r"tpl1714701190630.png", record_pos=(0.26, 0.113), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714701190630.png", record_pos=(0.26, 0.113), resolution=(2400, 1080)))
+            print("進入遊戲[10022]")
+        
+        case "10023":
+            while not exists(Template(r"tpl1714701225000.png", record_pos=(0.059, -0.028), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714701225000.png", record_pos=(0.059, -0.028), resolution=(2400, 1080)))
+            print("進入遊戲[10023]")
+        
+        case "10038":
+            while not exists(Template(r"tpl1714701612737.png", record_pos=(0.237, 0.07), resolution=(2400, 1080))):
+                swipe((0.8, 0.30), (0.2, 0.30), 0.6)
+            touch(Template(r"tpl1714701612737.png", record_pos=(0.237, 0.07), resolution=(2400, 1080))) 
+            print("進入遊戲[10038]")
+            while exists(Template(r"tpl1715239401696.png", record_pos=(-0.001, 0.181), resolution=(2400, 1080))):
+                touch((1200,960))
 # 點Spin
 def Spin():
     touch((1980, 1000))
@@ -169,8 +262,8 @@ def GameTest():
 
 # ------------------------------------------------------------------launch
 # 全遊戲巡測
-# for x in GameCode:
-#     ClickGame(x)
-#     GameTest()
+for x in GameCode:
+    ClickGameNew(x)
+    GameTest()
 
 
